6c4f2abe5ce2ef9553068a8e185f78ce28cf7fac
Rasquinha_ <116691516+Bruno-rasq@users.noreply.github.com>
2024-03-25 19:46:10 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { LinkedList } from './Data-structure/linkedlist';
+
+const list = new LinkedList();
+
+list.Add(2)
+list.Add(4)
+list.Add(1)
+list.Add(6)
+list.Add(7)
+
+list.Print()
+// console.log(list.Length())
+
+// list.Remove(2)
+// list.Remove(4)
+// list.Remove(1)
+
+// list.Print()
+// console.log(list.Length())
+
+// list.Get(4)
+// list.Get(8)
